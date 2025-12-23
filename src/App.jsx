c2d8b3222c7c974382fd6dbe5312914add327223
Ranjeet Vishwakarma <rv238502@gmail.com>
2025-12-23 13:46:55 +0530

    Fixed the bug by importing the right component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import Cocktails from "./components/cocktails";
+import Cocktails from "./components/Cocktails";
 import About from "./components/About";
 import Art from "./components/Art";
 import Menu from "./components/Menu";
